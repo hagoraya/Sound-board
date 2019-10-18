22b47fb6956b990cd360c3d1788216a370397891
Harry Singh <harpreetgoraya95@gmail.com>
2019-10-18 13:58:29 -0400

    New sound is created only when create button is clicked inside dialog box

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 //Waits for all elements on the page to load before running any functions
 $(document).ready(function () {
 
-
     //Initialize the dialog box
     $("#CreateNewSoungDialog").dialog({
         autoOpen: false,
         modal: true,
         draggable: true,
         title: "Create Sound",
+        width: 450,
+        height: 200,
         show: {
             effect: "blind",
             duration: 500
@@ -16,33 +17,27 @@ $(document).ready(function () {
             effect: "explode",
             duration: 500
         }
-        
     });
-
-    var buttonCount = 0;
-    //add new button when addButton is pressed (use # when refering to id) 
+    var buttonCount = 1;
+    //Open up a model dialog box when addButton is pressed
     $("#addButton").click(function () {
-        buttonCount++;
-
-        //use . when refering to class
-        $(".keys").append('<div class="buttons" > <button id=soundButton' + buttonCount + '> Sound ' + buttonCount + ' </button> </div>');
+        //Opens the dialogbox
+        $("#CreateNewSoungDialog").dialog("open");
 
     });
-
-
 
 
     //Open the dialog box when add new sound is clicked
-    $("#addButton").click(function (){
-        $("#CreateNewSoungDialog").dialog("open");
-    });
 
-    $('#CreateSoundButton').on('click', function () {
+    $('.CreateSoundButton').on('click', function () {
+        //Replace text when create button in dialog box is clicked (remove after)
         $('.myTarget').text($('.myInput').val());
+        //Close dialogbox if close button is clicked in side the dialog box
         $("#CreateNewSoungDialog").dialog('close');
+        //Create a new soundButton when when create button is clicked
+        $(".keys").append('<div class="buttons" > <button id=soundButton' + buttonCount + '> Sound ' + buttonCount + ' </button> </div>');
+        //increment buttoncount so we can keep track  of how many buttons we have made
+        buttonCount++;
     });
-
-   
 
 });
-
