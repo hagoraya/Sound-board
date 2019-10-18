3a1d35f52a5ee85578aeebc6abf1864077876d63
Harry Singh <harpreetgoraya95@gmail.com>
2019-10-18 13:16:28 -0400

    Create a modal popup when add sound is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
 //Waits for all elements on the page to load before running any functions
-$(document).ready(function(){ 
+$(document).ready(function () {
+
+
+    //Initialize the dialog box
+    $("#CreateNewSoungDialog").dialog({
+        autoOpen: false,
+        modal: true,
+        draggable: true,
+        title: "Create Sound",
+        show: {
+            effect: "blind",
+            duration: 500
+        },
+        hide: {
+            effect: "explode",
+            duration: 500
+        }
+        
+    });
 
     var buttonCount = 0;
-   //add new button when addButton is pressed (use # when refering to id) 
-   $("#addButton").click(function(){
-       buttonCount++;
+    //add new button when addButton is pressed (use # when refering to id) 
+    $("#addButton").click(function () {
+        buttonCount++;
 
-       //use . when refering to class
-        $(".keys").append('<div class="buttons" > <button id=soundButton'+buttonCount+'> Sound '+buttonCount+' </button> </div>');
+        //use . when refering to class
+        $(".keys").append('<div class="buttons" > <button id=soundButton' + buttonCount + '> Sound ' + buttonCount + ' </button> </div>');
 
-   });
-    
+    });
 
 
-      
+
+
+    //Open the dialog box when add new sound is clicked
+    $("#addButton").click(function (){
+        $("#CreateNewSoungDialog").dialog("open");
+    });
+
+    $('#CreateSoundButton').on('click', function () {
+        $('.myTarget').text($('.myInput').val());
+        $("#CreateNewSoungDialog").dialog('close');
+    });
+
+   
 
 });
 
