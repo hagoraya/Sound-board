759bcefce4755e59c2c7d5e89c0d848d958ac9d8
Harry Singh <harpreetgoraya95@gmail.com>
2019-10-18 16:10:09 -0400

    Fixed jQuery style errors
    jkhgjhghjgjyfjhgf

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
 //Waits for all elements on the page to load before running any functions
 $(document).ready(function () {
 
+    //Count for our sound button
+    var buttonCount = 1;
+
     //Initialize the dialog box
     $("#CreateNewSoungDialog").dialog({
         autoOpen: false,
+        show: "clip",
         modal: true,
         draggable: true,
         title: "Create Sound",
-        width: 450,
-        height: 200,
+        autoResize:true,
+        background: 'black',
+        position: 'center',
+        closeText: 'X',
         show: {
             effect: "blind",
             duration: 500
@@ -16,7 +22,21 @@ $(document).ready(function () {
         hide: {
             effect: "explode",
             duration: 500
+        },
+
+        buttons: {
+            "Create!": function () {
+                //Replace text when create button in dialog box is clicked (remove after)
+                $('.myTarget').text($('.myInput').val());
+                //Close dialogbox if close button is clicked in side the dialog box
+                $("#CreateNewSoungDialog").dialog('close');
+                //Create a new soundButton when when create button is clicked
+                $(".keys").append('<div class="buttons" > <button id=soundButton' + buttonCount + '> Sound ' + buttonCount + ' </button> </div>');
+                //increment buttoncount so we can keep track  of how many buttons we have made
+                buttonCount++;
+            }
         }
+
     });
     var buttonCount = 1;
     //Open up a model dialog box when addButton is pressed
@@ -25,19 +45,4 @@ $(document).ready(function () {
         $("#CreateNewSoungDialog").dialog("open");
 
     });
-
-
-    //Open the dialog box when add new sound is clicked
-
-    $('.CreateSoundButton').on('click', function () {
-        //Replace text when create button in dialog box is clicked (remove after)
-        $('.myTarget').text($('.myInput').val());
-        //Close dialogbox if close button is clicked in side the dialog box
-        $("#CreateNewSoungDialog").dialog('close');
-        //Create a new soundButton when when create button is clicked
-        $(".keys").append('<div class="buttons" > <button id=soundButton' + buttonCount + '> Sound ' + buttonCount + ' </button> </div>');
-        //increment buttoncount so we can keep track  of how many buttons we have made
-        buttonCount++;
-    });
-
 });
